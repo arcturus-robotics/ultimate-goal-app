28d702fb3de30b11266b41676a7c4dae797eb87a
wackbyte <wackbyte@pm.me>
2021-01-30 11:33:37 -0500

    change vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/VisionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/VisionAuto.java
@@ -15,11 +15,11 @@ public class VisionAuto extends LinearOpMode {
         // vision (eyeball) but awesome
         vision = new ArcturusVision(hardwareMap);
 
-        /**
-         * Activate TensorFlow Object Detection before we wait for the start command.
-         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-         **/
         if (vision.tfod != null) {
+            /**
+            * Activate TensorFlow Object Detection before we wait for the start command.
+            * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+            **/
             vision.tfod.activate();
             // The TensorFlow software will scale the input images from the camera to a lower resolution.
             // This can result in lower detection accuracy at longer distances (> 55cm or 22").
@@ -30,17 +30,15 @@ public class VisionAuto extends LinearOpMode {
 
             // Uncomment the following line if you want to adjust the magnification and/or the aspect ratio of the input images.
             //tfod.setZoom(2.5, 1.78);
-        }
 
-        telemetry.addData("sdjflkasfjsdlaf", "press play or you have 5 seconds to live");
-        telemetry.update();
+            telemetry.addData("sdjflkasfjsdlaf", "press play or you have 5 seconds to live");
+            telemetry.update();
 
-        waitForStart();
+            waitForStart();
 
-        // for some reason in the original example this was inside an if statement with the exact same condition which is pretty weird tbh
-        // so i removed it
-        while (opModeIsActive()) {
-            if (vision.tfod != null) {
+            // for some reason in the original example this was inside an if statement with the exact same condition which is pretty weird tbh
+            // so i removed it
+            while (opModeIsActive()) {
                 // getUpdatedRecognitions() will return null if no new information is available since
                 // the last time that call was made (quite unfortunate)
                 List<Recognition> updatedRecognitions = vision.tfod.getUpdatedRecognitions();
@@ -49,19 +47,19 @@ public class VisionAuto extends LinearOpMode {
                     // step through the list of recognitions and display boundary info
                     int i = 0;
                     for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
+                        if (recognition.getLabel() == "quad") {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                        }
                         i += 1;
                     }
                     telemetry.update();
                 }
             }
-        }
 
-        if (vision.tfod != null) {
             vision.tfod.shutdown();
         }
     }
