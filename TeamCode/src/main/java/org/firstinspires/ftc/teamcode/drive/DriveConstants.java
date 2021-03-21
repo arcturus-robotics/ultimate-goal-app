572ceff02c089d425278745f8a3b3a6c22f4c150
wackbyte <wackbyte@pm.me>
2021-03-21 14:19:13 -0400

    even more customer and remeasure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -51,7 +51,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 3; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 14.1220472; // in
+    public static double TRACK_WIDTH = 16.1641634; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -29,13 +29,13 @@ import java.util.List;
 @Config
 public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 8192;
-    public static double WHEEL_RADIUS = 2; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
+    public static double WHEEL_RADIUS = 0.984251969; // in
+    public static double GEAR_RATIO = 1.0; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 17.75; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -11.25; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 14.8125; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -8.40625; // in; offset of the lateral wheel
 
-    public static double X_MULTIPLIER = 0.9979209979209979;
+    public static double X_MULTIPLIER = 1.0;
     public static double Y_MULTIPLIER = 1.0;
 
     private final Encoder leftEncoder;
